38fb3c088c24c98d9b171f724031f9a94322578c
iblurdesigner <nevomen@gmail.com>
2024-01-30 20:22:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+import express, { json } from 'express'
+import { createMovieRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
+import { PORT } from './config.js'
+
+export const createApp = ({ movieModel }) => {
+  const app = express()
+  // const PORT = process.env.PORT ?? 1234
+  app.disable('x-powered-by')
+  app.use(json())
+  app.use(corsMiddleware())
+
+  // Todos los recursos que sean MOVIES se identifican con /movies
+  app.use('/movies', createMovieRouter({ movieModel }))
+
+  // puerto del servidor
+  app.listen(PORT, () => {
+    console.log(`Server listening on port http://localhost:${PORT}`)
+  })
+}
